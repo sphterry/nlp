c42adff7d09b50e139a53eb9f20d94cf17cc1ae4
st275 <st275@st-andrews.ac.uk>
2026-02-01 12:43:29 +0000

    Question 5

---
+++ src/week2tut1/regex_example.py
@@ -0,0 +1,11 @@
+from nltk import *
+
+REGEX = [r"[A-Za-z]+", # words with only letters
+         r"[A-Z][a-z]*", # title case words
+         r"\d+(\.\d+)?", # ints and floats
+         r"p[aeiou]{,2}t"] # words that start with p and ending in t, with an option to have one or two lowercase vowels in the middle
+
+TEXT = "Check answers in Python 109, 1.09 patch pitch pt piiiit"
+
+for pattern in REGEX:
+    re_show(pattern, TEXT)
